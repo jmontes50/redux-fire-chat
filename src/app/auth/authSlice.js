d06f16e8ab03308546026c4ccf538e7824ad082e
Osmar Montesinos <osmarmontes50@gmail.com>
2024-09-04 20:06:33 -0500

    added extraReducers auth

--- src/app/auth/authSlice.js
+++ src/app/auth/authSlice.js
@@ -33,6 +33,7 @@ const authSlice = createSlice({
   initialState: {
     user: null,
     isLoading: true,
+    error: null
   },
   reducers: {
     setUser: (state, action) => {
@@ -42,6 +43,21 @@ const authSlice = createSlice({
       state.isLoading = action.payload;
     },
   },
+  extraReducers: (builder) => {
+    builder
+    .addCase(signInWithGoogle.pending, (state) => {
+      state.isLoading = true;
+      state.error = null;
+    })
+    .addCase(signInWithGoogle.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.user = action.payload;
+    })
+    .addCase(signInWithGoogle.rejected, (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    })
+  }
 });
 
 export const { setUser } = authSlice.actions;
